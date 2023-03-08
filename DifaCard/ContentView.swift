5ae576f590a55034b7935db8cb1dfa9798ba3c73
Muhammad Difa Sulthon Diani <muhammad.diani@btpn.com>
2023-03-08 20:33:12 +0700

    Initial Commit

---
+++ DifaCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DifaCard
+//
+//  Created by MUHAMMAD DIFASHULTON DIANI on 08/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DifaCard/DifaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DifaCardApp.swift
+//  DifaCard
+//
+//  Created by MUHAMMAD DIFASHULTON DIANI on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DifaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
